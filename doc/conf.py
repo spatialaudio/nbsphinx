a326f879c5aeab6bcf4ed5f95075a3b6f69fef7a
Matthias Geier <Matthias.Geier@gmail.com>
2015-12-23 19:19:10 +0100

    Re-order stuff in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -4,11 +4,10 @@
 # Use sphinx-quickstart to create your own conf.py file!
 # After that, you have to edit a few things.  See below.
 
-import sys
 import os
-from subprocess import check_output
+import sys
 
-# Add path to nbsphinx.py (if you didn't install it with pip):
+# Add relative path to nbsphinx.py (if you didn't install it with pip):
 sys.path.insert(0, os.path.abspath('..'))
 
 # Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
@@ -17,12 +16,16 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
 # *** the following lines are (hopefully) temporary,
 # see https://github.com/sphinx-doc/sphinx/issues/2162
-import nbsphinx
 source_suffix = ['.rst', '.ipynb']
-source_parsers = {'ipynb': nbsphinx.NotebookParser}
+source_parsers = {'ipynb': 'nbsphinx.NotebookParser'}
 # *** end of temporary lines
+
+# -- The settings below this line are not specific to nbsphinx ------------
 
 master_doc = 'index'
 
@@ -30,15 +33,14 @@ project = 'nbsphinx'
 author = 'Matthias Geier'
 copyright = '2015, ' + author
 
-# Get version information from Git:
+# -- Get version information from Git -------------------------------------
+
 try:
+    from subprocess import check_output
     release = check_output(['git', 'describe', '--tags', '--always'])
     release = release.decode().strip()
 except Exception:
     release = '<unknown>'
-
-# Exclude build directory and Jupyter backup files:
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # -- Options for HTML output ----------------------------------------------
 
