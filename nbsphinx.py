51ca141e089227d5647a3a3f8a84918fccf2d118
Matthias Geier <Matthias.Geier@gmail.com>
2016-12-23 12:25:20 +0100

    CSS: Disable scrollbars on prompts

--- nbsphinx.py
+++ nbsphinx.py
@@ -363,6 +363,12 @@ div.nboutput div.prompt {
     }
 }
 
+/* disable scrollbars on prompts */
+div.nbinput div.prompt pre,
+div.nboutput div.prompt pre {
+    overflow: hidden;
+}
+
 /* input/output area */
 div.nbinput div.input_area,
 div.nboutput div.output_area {
