2a34d1abe575494fcb1768a18e4c15c02f22f53f
Matthias Geier <Matthias.Geier@gmail.com>
2018-11-25 18:50:56 +0100

    Set minimum supported Sphinx version to 1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'nbconvert!=5.4',
         'traitlets',
         'nbformat',
-        'sphinx>=1.4',
+        'sphinx>=1.6',
     ],
     author='Matthias Geier',
     author_email='Matthias.Geier@gmail.com',
