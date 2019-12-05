072c6b7d338309a34bb7b072c958516e95c5f3fa
Matthias Geier <Matthias.Geier@gmail.com>
2019-12-05 19:56:26 +0100

    DOC: Switch to sphinxdoc theme

--- doc/conf.py
+++ doc/conf.py
@@ -140,6 +140,22 @@ except Exception:
 
 html_favicon = 'favicon.svg'
 html_title = project + ' version ' + release
+html_theme = 'sphinxdoc'
+html_theme_options = {
+    #'nosidebar': True,
+    #'sidebarwidth': 300,
+    'body_max_width': None,
+    'navigation_with_keys': True,
+}
+html_sidebars = {
+    '**': [
+        'globaltoc.html',
+        #'localtoc.html',
+        'relations.html',
+        'sourcelink.html',
+        'searchbox.html',
+    ]
+}
 
 # -- Options for LaTeX output ---------------------------------------------
 
