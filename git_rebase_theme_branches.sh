87d9e2a581c6e64cb909b75ca2159796a7ddc151
Matthias Geier <Matthias.Geier@gmail.com>
2023-09-03 13:20:13 +0200

    git rebase script: wait 10 minutes between builds

--- git_rebase_theme_branches.sh
+++ git_rebase_theme_branches.sh
@@ -6,6 +6,8 @@ fmt='
 git checkout %(refname)
 git rebase master
 git push origin HEAD:%(refname:strip=3) --force
+echo "giving RTD some time to build the docs"
+sleep 10m
 '
 
 eval "$(git for-each-ref --shell --format="$fmt" refs/remotes/origin/*-theme)"
